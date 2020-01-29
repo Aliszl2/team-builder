f7752b04646b422843db409bbf9e1f1ba3c83319
Alison Law <alisparklaw@gmail.com>
2020-01-29 13:25:03 +0000

    Team List skeleton build

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,57 +1,67 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import TeamMembers from "./TeamMembers";
 
 const Form = () => {
- const [teamMember, setTeamMember]=useState({
-    firstName:"",
-    lastName:"",
-    email:"",
- })
+  const [teamMember, setTeamMember] = useState({
+    firstName: "",
+    lastName: "",
+    email: ""
+  });
+  const [submit, setSubmit] = useState([]);
 
   return (
-    <StyledDiv>        <h1>Enter Team Member:</h1>
-      <form action="">
-        <label htmlFor="teamMember_firstName">First name </label>
-        <input
-          type="text"
-          name="firstName"
-          id="teamMember_firstName"
-          placeholder="first name"
-        /><br/>
-         <label htmlFor="teamMember_lastName">Last name </label>
-        <input
-          type="text"
-          name="lastName"
-          id="teamMember_lastName"
-          placeholder="last name"
-        /><br/>
-          <label htmlFor="teamMember_email">Work  email</label>
-        <input
-          type="email"
-          name="email"
-          id="teamMember_email"
-          placeholder="email"
-        /><br/>
-               <label htmlFor="teamMember_role">Team role </label>
-           <input
-          type="text"
-          name="role"
-          id="teamMember_role"
-          placeholder="team role"
-        />
-      </form>
+    <StyledDiv>
+      <div>
+        <h1>Enter Team Member:</h1>
+        <form action="">
+          <label htmlFor="teamMember_firstName">First name </label>
+          <input
+            type="text"
+            name="firstName"
+            id="teamMember_firstName"
+            placeholder="first name"
+          />
+          <br />
+          <label htmlFor="teamMember_lastName">Last name </label>
+          <input
+            type="text"
+            name="lastName"
+            id="teamMember_lastName"
+            placeholder="last name"
+          />
+          <br />
+          <label htmlFor="teamMember_email">Work email</label>
+          <input
+            type="email"
+            name="email"
+            id="teamMember_email"
+            placeholder="email"
+          />
+          <br />
+          <label htmlFor="teamMember_role">Team role </label>
+          <input
+            type="text"
+            name="role"
+            id="teamMember_role"
+            placeholder="team role"
+          />
+        </form>
+      </div>
+      <TeamMembers />
     </StyledDiv>
   );
 };
 
 export default Form;
 
-
 const StyledDiv = styled.div`
-border:5px black solid;
-width:700px;
-margin: 0 auto;
-form{
-    margin:50px;
-}
-`
+  div {
+    border: 5px black solid;
+    width: 700px;
+    margin: 0 auto;
+    form {
+      margin: 50px;
+    }
+  }
+`;
---
+++ team-builder/src/components/TeamMembers.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const TeamMembers=(props)=>{
+
+return(
+    <h1>Team members list</h1>
+)
+
+}
+
+export default TeamMembers;
